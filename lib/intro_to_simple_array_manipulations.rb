2d9e484874c036004b1af3f24dfb53621cf14dbc
Austin Kohler <austinckohler@gmail.com>
2020-01-24 18:30:39 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -40,3 +40,4 @@ end
 def using_insert(list_of_programming_languages, another_language)
   @list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
     @another_language = "Python"
+    @list_of_programming_languages.insert(@another_language)
