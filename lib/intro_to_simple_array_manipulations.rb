a278c20745d903c7e37bd75bb154e9a57fe4fbc4
Austin Kohler <austinckohler@gmail.com>
2020-01-24 18:25:48 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -36,3 +36,5 @@ def using_concat(my_favorite_things, more_favs)
   @more_favs = ["sports cars", "flatiron school"]
   @all_my_favs = @my_favorite_things.concat(@more_favs)
 end
+
+def using_insert(list_of_programming_languages, another_language)
