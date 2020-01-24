fc50e1d8082ae4635e9cf4b678a76eac36d7becc
Austin Kohler <austinckohler@gmail.com>
2020-01-24 17:19:47 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -17,5 +17,5 @@ def using_pop(continents)
 end
 
 def pop_with_args(dog_breeds)
-   @dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
-  @dog_breeds.pop(2)
+   dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+    small_dogs = (2)
