bff8bab2e3bc2fdc7b83313b03a827b08dc7dbfb
Austin Kohler <austinckohler@gmail.com>
2020-01-24 18:36:17 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -54,4 +54,6 @@ def using_flatten(instruments)
 end
 
 def using_delete(instructors, "Steven")
-  
+  instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
+  instructors.delete
+end
